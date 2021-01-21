13ed77c391cf4ce8da8d5a2b5fa96fa267f1a614
RikeshThapa <rthapa@u.rochester.edu>
2021-01-21 03:05:03 -0600

    Basics of eventsRoutes setup

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const eventRoutes = require('./routes/events-routes');
+
 const app = express();
 
+app.use(eventRoutes);
 
 app.listen(5000);
--- routes/events-routes.js
+++ routes/events-routes.js
@@ -1,1 +1,10 @@
 const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    console.log('GET Request in events');
+    res.json({message: "Getting all events"});
+});
+
+module.exports = router;
