84d2679908b1c4528caad0e574a4209e316051d0
RikeshThapa <rthapa@u.rochester.edu>
2021-02-12 04:30:28 -0600

    adding PORT management

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const options = {
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.u3lfs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, options)
     .then(() => {
-        app.listen(5000);
+        app.listen(process.env.PORT || 5000);
     })
     .catch(err => {
         console.log(err);
