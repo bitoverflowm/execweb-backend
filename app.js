56cba8106362402bf70aaa1d85b45a063cfd88f3
EduardoCL <35414231+EduardoCortesL@users.noreply.github.com>
2021-02-18 00:16:31 -0800

    db running

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const eventRoutes = require('./routes/events-routes');
@@ -12,6 +13,16 @@ const app = express();
 const HttpError = require('./models/http-error');
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+});
 
 app.use('/api/events', eventRoutes);
 
@@ -35,4 +46,20 @@ app.use((error, req, res, next) => {
     res.status(error.code || 500).json({ message: error.message || 'An unknown error occurred!'});
 });
 
-app.listen(5000);
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+}
+
+mongoose
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.u3lfs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, options)
+    .then(() => {
+        app.listen(process.env.PORT || 5000);
+        console.log("Connected to Db")
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
